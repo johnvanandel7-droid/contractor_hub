2a788332ce97c50e5231fd922b974d229a7a499f
John van Andel <johnvanandel7@gmail.com>
2026-05-28 06:07:50 -0400

    activating firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:contractor_hub/screens/welcome%20screen.dart';
 import 'package:contractor_hub/screens/to_do_list.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'screens/home_page.dart';
 import 'screens/clock_in_out.dart';
 import 'screens/login_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
