d88fd031fca35796783c9f80939cb86944c54461
John van Andel <johnvanandel7@gmail.com>
2026-08-12 15:43:44 -0400

    fixing todo screen

--- lib/screens/to_do_list.dart
+++ lib/screens/to_do_list.dart
@@ -1,5 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:contractor_hub/components/go_home.dart';
+import 'package:contractor_hub/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+final auth = FirebaseAuth.instance;
+final firebase = FirebaseFirestore.instance;
 
 class ToDoList extends StatefulWidget {
   const ToDoList({super.key});
@@ -9,7 +15,19 @@ class ToDoList extends StatefulWidget {
 }
 
 class _ToDoListState extends State<ToDoList> {
-  List toDoItems = [];
+  TextEditingController newTaskController = TextEditingController();
+  DateTime? _completionDate;
+
+  final String currentUserUid = auth.currentUser!.uid;
+
+  Future<void> _pickDate(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(context: context, initialDate: _completionDate ?? DateTime.now(), firstDate: DateTime(2000), lastDate: DateTime(2100));
+    if (pickedDate != null && pickedDate != _completionDate) {
+      setState(() {
+        _completionDate = pickedDate;
+      });
+    }
+  }
 
   Future<void> _openNewTask() async {
     await showBottomSheet(
@@ -20,7 +38,33 @@ class _ToDoListState extends State<ToDoList> {
       builder: (context) {
         return SingleChildScrollView(
           child: Column(
-            children: [Text('New Task'), SizedBox(height: 20), TextField()],
+            children: [
+              Text('New Task'),
+              SizedBox(height: 20),
+              TextField(
+                controller: newTaskController,
+                decoration: kInputDecoration.copyWith(
+                  hintText: 'take out garbage',
+                ),
+              ),
+              SizedBox(height: 10),
+              Text('Completion date'),
+              MaterialButton(onPressed: () => _pickDate(context), child: Container(decoration: BoxDecoration(color: Colors.grey,), child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Choose Completion date'),
+              ),)),
+              ElevatedButton(
+                onPressed: () async {
+                  Navigator.pop(context);
+                  await firebase.collection('ToDoItems').add({
+                    'message': newTaskController.text,
+                    'createdAt': DateTime.now,
+                    'completionDate': 
+                  });
+                },
+                child: Text('Save'),
+              ),
+            ],
           ),
         );
       },
