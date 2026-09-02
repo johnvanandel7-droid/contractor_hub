470ac155cb436004530a8aaafe31321c32fa13fb
John van Andel <johnvanandel7@gmail.com>
2026-09-02 09:18:19 -0400

    fixing weird problems

---
+++ lib/screens/administrator_panel.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdministratorPanel extends StatelessWidget {
+  const AdministratorPanel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+        Text('Administrator panel'),
+        SizedBox(height: ,)
+      ],)
+    );
+  }
+}
